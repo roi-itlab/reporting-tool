0a1a120bf575f9cf79385078f002815450af3405
Helen <el.tchekal2011@yandex.ru>
2018-12-07 12:34:59 +0300

    Add files via upload
    Legend isn't working. BarChartEdit - version  without comments.  BarChart - with comments

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <BarChart></BarChart>
+     <!--<BarChart width_chart = 900></BarChart>-->
+  </div>
+</template>
+
+<script>
+import BarChart from './components/BarChart'
+
+export default {
+  name: 'App',
+  components: {
+    BarChart
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
